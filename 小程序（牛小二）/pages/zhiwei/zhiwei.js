e152ba63a3860ed60dbd03492d1e8fe94e82bdff
剑 <49889491+729264569@users.noreply.github.com>
2020-06-04 11:53:56 +0800

    Add files via upload

---
+++ 小程序（牛小二）/pages/zhiwei/zhiwei.js
@@ -0,0 +1,89 @@
+// pages/zhiwei/zhiwei.js
+Page({
+ 
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    waA: {
+      a: '日照顺启物流直招',
+      b: '04-26',
+      c: '17000-20000/日',
+      d: '山东省 日照市',
+      e: 'A2驾照',
+      www:'../bl/bl'
+    },
+    waB: {
+      a: '招A2驾照员一名',
+      b: '04-26',
+      c: '面试/月',
+      d: '江苏省 江阴市',
+      e: 'A2驾照',
+        www: '../bl/bd'
+    },
+    waC: {
+      a: '诚招挂车司机',
+      b: '04-26',
+      c: '8000-10000/月',
+      d: '广东省 深圳市',
+      e: 'A2驾照',
+        www: '../bl/bq'
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
